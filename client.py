f7e0e25cd09da05b1e5c6bb77b170e4b0758c6f9
khalil gardouh <khalilgardouh@khalils-MacBook-Pro.local>
2018-10-17 19:41:15 +0200

    Update

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ TCP_IP = "37.59.57.203"
 
 TCP_PORT = 55555
 
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 2048
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,4 +16,17 @@ data = s.recv(BUFFER_SIZE)
 
 print (data)
 
-manfiest={}
+manfiest="{}"
+
+with open("manifest.json", "r+") as f:
+    manfiest = json.load(f)
+s.send(json.dumps(manfiest).encode('utf-8'))
+
+data = s.recv(BUFFER_SIZE)
+
+print (data)
+
+s.close()
+
+
+
